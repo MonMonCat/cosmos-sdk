82cce79295ffb30ca62c7fca6a2bd3dbc8411b3c
Magic Cat <pusz.monika@gmail.com>
2022-02-28 14:00:10 +0000

    rm panic from registerImpl err

--- codec/types/interface_registry.go
+++ codec/types/interface_registry.go
@@ -148,16 +148,15 @@ func (registry *interfaceRegistry) registerImpl(iface interface{}, typeURL strin
 	// we panic).
 	foundImplType, found := imap[typeURL]
 	if found && foundImplType != implType {
-		panic(
-			fmt.Errorf(
-				"concrete type %s has already been registered under typeURL %s, cannot register %s under same typeURL. "+
-					"This usually means that there are conflicting modules registering different concrete types "+
-					"for a same interface implementation",
-				foundImplType,
-				typeURL,
-				implType,
-			),
+		fmt.Errorf(
+			"concrete type %s has already been registered under typeURL %s, cannot register %s under same typeURL. "+
+				"This usually means that there are conflicting modules registering different concrete types "+
+				"for a same interface implementation",
+			foundImplType,
+			typeURL,
+			implType,
 		)
+		return
 	}
 
 	imap[typeURL] = implType
